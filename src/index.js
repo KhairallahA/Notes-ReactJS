113bb5780d519adb8f0a25f9f592c20ec6f63356
Khairallah AL-Awady <khairallahfreelancer@gmail.com>
2021-09-10 20:55:38 +0300

    Add Notes-ReactJS Project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom, { render } from 'react-dom';
+import App from './App'
+
+ReactDom.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
